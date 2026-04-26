cf73b8716a13aa5d3a4af199947509068902f001
Ashu <ashuv6309@gmail.com>
2026-04-27 00:02:48 +0530

    solve bottom view question - GFG

--- binary-Trees/bottom_view.py
+++ binary-Trees/bottom_view.py
@@ -1,17 +1,18 @@
 from collections import deque
-def top_view(node):
+def bottom_view(node):
     queue = deque([])
     result = {}
     queue.append([node, 0])
     while queue:
         elem, line = queue.popleft()
-        if line not in result:
-            result[line] = elem.val
+        result[line] = elem.val
         if elem.left:
             queue.append([elem.left, line-1])
         if elem.right:
             queue.append([elem.right, line+1])
+        
     ans = []
-    for value in sorted(result.values()):
-        ans.append(value)
+    for value in sorted(result.items()):
+        ans.append(value[1])
+
     return ans
