ac1274b75b1843b128cb23d114ba4b67812cd2f1
Ashu <ashuv6309@gmail.com>
2026-07-17 17:05:44 +0530

    solve 27 Remove Elements - leetcode

---
+++ patterns/two_pointers/27remove_element.py
@@ -0,0 +1,14 @@
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+
+# keeping the good elements at the starting of the list
+def remove_element(nums, val):
+    k = 0
+
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+    return k
+
+print(remove_element(nums, val))
