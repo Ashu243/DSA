c5714bfe78da97886ce32d603c6148f1e48d634a
Ashu <ashuv6309@gmail.com>
2026-07-18 21:08:32 +0530

    solve remove duplicates from sorted array 2 - leetcode

---
+++ patterns/two_pointers/80_remove_duplicates2.py
@@ -0,0 +1,20 @@
+nums = [0,0,1,1,1,1,2,3,3]
+
+
+def remove_duplicates(nums):
+    j = 1
+    count = 1
+    n = len(nums)
+
+    for i in range(1, n):
+        if nums[i] == nums[i-1]:
+            count += 1
+        else:
+            count = 1
+        if count <= 2:
+            nums[j] = nums[i]
+            j+= 1
+    return j
+
+print(remove_duplicates(nums))
+            
