1b80680cd65d43c89c710208d16f035c74c861d0
Ashu <ashuv6309@gmail.com>
2026-08-19 23:20:42 +0530

    solve 28. Find the Index of the First Occurrence in a String

--- patterns/all_pattern_revision/28_first_occ.py
+++ patterns/all_pattern_revision/28_first_occ.py
@@ -7,6 +7,10 @@ def first_occ(haystack, needle):
     if needle in haystack:
         for i in range(len(haystack)):
             if needle[0] == haystack[i]:
-                return i
-    
+                j = 0
+                while j < len(needle) and haystack[i+j] == needle[j]:
+                    j += 1
+                if j == len(needle):
+                    return i
+            
     return -1
