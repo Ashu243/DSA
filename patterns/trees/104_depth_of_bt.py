21d79aecbf745c2c0378d2a8d8d7e4d26d915b44
Ashu <ashuv6309@gmail.com>
2026-08-24 22:17:06 +0530

    solve 104. Maximum Depth of Binary Tree

---
+++ patterns/trees/104_depth_of_bt.py
@@ -0,0 +1,24 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, node) -> int:
+        queue = deque([node])
+        height = 0
+
+        while queue:
+            height += 1
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return height
+        
